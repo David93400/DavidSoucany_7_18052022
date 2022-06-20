b399be4c7b41ca6a92380931f7c0e3f845192d2f
David Soucany <david.soucany@bloomays.com>
2022-06-20 17:14:52 +0200

    WIP algo 1

--- src/scripts/utils/filter.js
+++ src/scripts/utils/filter.js
@@ -1,5 +1,10 @@
 import { displayInputOption } from '../pages/home';
-import { cleanError, createGenericElement, normalizeText } from './helpers';
+import {
+  cleanError,
+  createGenericElement,
+  normalizeText,
+  uniqueArray,
+} from './helpers';
 
 const handleArrow = (element, firstClass, secondClass) => {
   if (element.classList.contains(firstClass)) {
@@ -158,7 +163,31 @@ const searchOptionsByInput = (recipes, type) => {
 };
 
 const mainSearch = (recipes, keyword) => {
-  console.log(recipes, keyword);
+  let filteredUniqueRecipes = [];
+  let newRecipesList = [];
+  if (keyword.length === 0) {
+    cleanError('.error');
+    return;
+  }
+  if (keyword.length < 3) {
+    cleanError('.error');
+    const error = createGenericElement('div', '', 'error');
+    error.innerText = 'Veuillez entrer au moins 3 caractères';
+    document.querySelector('.search-section').appendChild(error);
+    return recipes;
+  }
+  cleanError('.error');
+  recipes.forEach((recipe) => {
+    if (
+      normalizeText(recipe.name).includes(normalizeText(keyword)) ||
+      normalizeText(recipe.description).includes(normalizeText(keyword)) ||
+      recipe.ingredients.toString().includes(normalizeText(keyword))
+    ) {
+      newRecipesList.push(recipe);
+      filteredUniqueRecipes = uniqueArray(newRecipesList);
+    }
+  });
+  return filteredUniqueRecipes;
 };
 
 const cleanRecipesCard = () => {
